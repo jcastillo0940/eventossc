8152a374f7ee696df3d928f7d255cb203a092abc
root <root@srv1504812.hstgr.cloud>
2026-03-23 13:27:08 +0000

    update gh

--- resources/views/layouts/app-dashboard.blade.php
+++ resources/views/layouts/app-dashboard.blade.php
@@ -7,7 +7,6 @@
     <title>Dashboard - @yield('title')</title>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
         :root {
             --sc-blue:   #1A6FBF;
